d5aa07847447ba8eafeffe9bd49722e941a1ca8e
Precial <ggrty10@gmail.com>
2021-10-29 12:34:19 +0900

    Success NaberLogin

---
+++ Login_Naver_Example/HomeViewController.swift
@@ -0,0 +1,24 @@
+//
+//  HomeViewController.swift
+//  Login_Naver_Example
+//
+//  Created by SG on 2021/10/29.
+//
+
+import UIKit
+import NaverThirdPartyLogin
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    @IBAction func logoutBtn(_ sender: Any) {
+    }
+    
+
+}
--- Login_Naver_Example/ViewController.swift
+++ Login_Naver_Example/ViewController.swift
@@ -5,15 +5,45 @@
 //  Created by SG on 2021/10/28.
 //
 
+import Foundation
 import UIKit
+import NaverThirdPartyLogin
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, NaverThirdPartyLoginConnectionDelegate {
+    
+    
+    let loginInstance = NaverThirdPartyLoginConnection.getSharedInstance()
+    
     override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        loginInstance?.delegate = self
     }
+    
+    // 로그인에 성공한 경우 호출
+    func oauth20ConnectionDidFinishRequestACTokenWithAuthCode() {
+        performSegue(withIdentifier: "successLogin", sender: nil)
+        print("Success login")
+    }
+    
+    // referesh token
+    func oauth20ConnectionDidFinishRequestACTokenWithRefreshToken() {
+        loginInstance?.accessToken
+    }
+    
+    // 로그아웃
+    func oauth20ConnectionDidFinishDeleteToken() {
+        print("log out")
+    }
+    
+    // 모든 error
+    func oauth20Connection(_ oauthConnection: NaverThirdPartyLoginConnection!, didFailWithError error: Error!) {
+        print("error = \(error.localizedDescription)")
+    }
+    
+    @IBAction func naverLoginBtn(_ sender: Any) {
+        loginInstance?.requestThirdPartyLogin()
+    }
+    
+  
 
-
+    
 }
-
